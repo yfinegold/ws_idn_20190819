339999aed875d0d2039b75b9a26ff8552e9d0512
Yelena <yelena.finegold@fao.org>
2019-09-29 14:53:30 +0200

    update pysmm

--- scripts/run_smm/download_pysmm.sh
+++ scripts/run_smm/download_pysmm.sh
@@ -10,8 +10,8 @@ source env/bin/activate
 pip install pysmm
 pip install ipyleaflet
 
-wget -O pysmm_upgrade_20190822.zip https://www.dropbox.com/s/4r7d93803tmr2qf/pysmm_upgrade_20190822.zip
-unzip -o pysmm_upgrade_20190822.zip -d env/lib/python2.7/site-packages/ 
+wget -O pysmm_upgrade_20190922.zip https://www.dropbox.com/s/nwil00kw407ock4/pysmm_upgrade_20190922.zip
+unzip -o pysmm_upgrade_20190922.zip -d env/lib/python2.7/site-packages/ 
 python -m ipykernel install --user --name=env  
 wget -O run_pysmm.ipynb https://goo.gl/NbpUWr
 deactivate
